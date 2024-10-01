785d9910e0975a7e06e7b2422fc34f274006dae5
javnik36 <javnik36@gmail.com>
2024-10-01 23:02:42 +0200

    remove redundant code chunks

--- ffkern.py
+++ ffkern.py
@@ -1,72 +1,20 @@
 import pathlib as pl
 import fontforge as ff
-#from PIL import Image as pim
 
-__ff_path__ = pl.Path("F:\FontForgeBuilds").joinpath("bin", "ffpython.exe")
 __font_path__ = pl.Path("Arkhamic.ttf")
 __glyphs_folder__ = pl.Path("glyphs")
-__kern_pairs_folder__ = pl.Path("kern-pairs")
-__glyph_pairs_folder__ = pl.Path("glyph-pairs")
-
-worth_glyphs = []
-
-font_holder = ff.open(str(__font_path__)) # ff.font obj
-for glyph in font_holder.glyphs():
-    if font_holder[glyph.glyphname].isWorthOutputting():
-        worth_glyphs.append(font_holder[glyph.glyphname])
-
-def generate_glyph_files():
-    for item in worth_glyphs:
-        glyph_name = ""
-        glyph_repr = repr(item).split(" ")
-        if glyph_repr[3].startswith("U+"):
-            glyph_name = glyph_repr[3]
-        else:
-            glyph_name = item.glyphname
-        #item.export(f"{str(__glyphs_folder__.joinpath(item.glyphname))}.png")
-        item.export(f"{str(__glyphs_folder__.joinpath(glyph_name))}.png")
-
-def generate_kern_pairs():
-    kerning_table = []
-    table_name="'kern' Horizontal Kerning lookup 0 subtable"
-    for glyph in worth_glyphs:
-        kernings = glyph.getPosSub(table_name)
-        for kerning in kernings:
-            if kerning[1] == "Pair":
-                # glyph1.name, glyph2.name, alignement
-                kerning_table.append((glyph.glyphname, kerning[2], kerning[5]))
-                # glyph1, glyph2, alignement
-                #kerning_table.append((glyph, kerning[2], kerning[5]))
-
-    #ff.printSetup('pdf-file','z.pdf',2000,2000)
-    #comd = "gswin64c -dSAFER -dBATCH -dQUIET -dNOPAUSE -dEPSCrop -r600 -sDEVICE=png256 -sOutputFile='sampel.png'"
-    #ff.printSetup('command',comd)
-    #ff.printSetup('ps-file',"z.ps",300,200)
-    print_sample = ""
-    divider = "    "
-    ff.printSetup('pdf-file',"z.pdf")
-    for kern in kerning_table:
-        #sample_text = f"{kern[0]}{kern[1]}"
-        #font_holder.printSample('chars', 0, sample_text, f"{str(__kern_pairs_folder__.joinpath(sample_text))}.png")
-        #font_holder.selection.select(kern[0])
-        #font_holder.selection.select(('more', None),kern[1])
-        ####1 file - 1 pair
-        #sample_text = f"{chr(font_holder[kern[0]].encoding)}{chr(font_holder[kern[1]].encoding)}"
-        #sample_name = f"{kern[0]}{kern[1]}"
-        #font_holder.printSample('fontsample', 0, sample_text, f"{str(__kern_pairs_folder__.joinpath(sample_name))}.ps")#remove sample text from .ps
-        #font_holder.selection.none()
-        print_sample += f"{chr(font_holder[kern[0]].encoding)}{chr(font_holder[kern[1]].encoding)}"
-        print_sample += divider
-    font_holder.printSample('fontsample', 14, print_sample, f"{str(__kern_pairs_folder__.joinpath('kern-pairs'))}.pdf")#remove sample text from .ps
 
 
-    #glyph.getPosSub('*')
-    #(("'kern' Horizontal Kerning lookup 0 subtable", 'Pair', 'W', 0, 0, 12, 0, 0, 0, 0, 0),)
-    #name,typeoftable,leter,>,>,horizontal_advance
-    #font.getLookupInfo
-    #font.printSample
+def import_font(fontpath=__font_path__):
+    worth_glyphs = []
 
-def remove_non_unicode_glyphs():
+    font_holder = ff.open(str(__font_path__)) # ff.font obj
+    for glyph in font_holder.glyphs():
+        if font_holder[glyph.glyphname].isWorthOutputting():
+            worth_glyphs.append(font_holder[glyph.glyphname])
+    return font_holder,worth_glyphs
+
+def remove_non_unicode_glyphs(worth_glyphs):
     glyph_pairs_worth = []
     for item in worth_glyphs:
         glyph_repr = repr(item).split(" ")
@@ -76,11 +24,44 @@ def remove_non_unicode_glyphs():
             glyph_pairs_worth.append(item)
     return glyph_pairs_worth
 
+def setup_printing():
+    ff.printSetup('pdf-file',"z.pdf")
 
-def generate_all_permutations():
+def generate_glyph_files(worth_glyphs, glyphs_folder=__glyphs_folder__):
+    for item in worth_glyphs:
+        glyph_name = ""
+        glyph_repr = repr(item).split(" ")
+        if glyph_repr[3].startswith("U+"):
+            glyph_name = glyph_repr[3]
+        else:
+            glyph_name = item.glyphname
+        item.export(f"{str(glyphs_folder.joinpath(glyph_name))}.png")
+
+def generate_kern_pairs(font_holder, worth_glyphs):
+    kerning_table = []
+    table_name="'kern' Horizontal Kerning lookup 0 subtable"
+    for glyph in worth_glyphs:
+        kernings = glyph.getPosSub(table_name)
+        for kerning in kernings:
+            if kerning[1] == "Pair":
+                # glyph1.name, glyph2.name, alignment
+                kerning_table.append((glyph.glyphname, kerning[2], kerning[5]))
+
+    print_sample = ""
+    divider = "    "
+    setup_printing()
+    for kern in kerning_table:
+        print_sample += f"{chr(font_holder[kern[0]].encoding)}{chr(font_holder[kern[1]].encoding)}"
+        print_sample += divider
+    font_holder.printSample('fontsample', 14, print_sample, "kerning-pairs.pdf")#remove sample text from .ps
+    #glyph.getPosSub('*')
+    #(("'kern' Horizontal Kerning lookup 0 subtable", 'Pair', 'W', 0, 0, 12, 0, 0, 0, 0, 0),)
+    #name,typeoftable,leter,>,>,horizontal_advance
+
+def generate_all_permutations(font_holder):
     glyph_pairs_worth = remove_non_unicode_glyphs()
 
-    ff.printSetup('pdf-file',"z.pdf")
+    setup_printing()
 
     print_sample = ""
     divider = "    "
@@ -90,19 +71,16 @@ def generate_all_permutations():
         for j in range(len(glyph_pairs_worth)-1):
             print_sample += f"{chr(glyph_pairs_worth[i].encoding)}{chr(glyph_pairs_worth[j].encoding)}"
             print_sample += divider
-            #print_sample = f"{chr(glyph_pairs_worth[i].encoding)}{chr(glyph_pairs_worth[j].encoding)}"
-            #sample_name = f"{glyph_pairs_worth[i].glyphname}{glyph_pairs_worth[j].glyphname}"
-            #font_holder.printSample('fontsample', 20, print_sample, f"{str(__glyph_pairs_folder__.joinpath(sample_name))}.ps")#remove sample text from .ps
 
-    font_holder.printSample('fontsample', 14, print_sample, f"{str(__glyph_pairs_folder__.joinpath('sample_graph_pairs'))}.pdf")#remove sample text from .ps
+    font_holder.printSample('fontsample', 14, print_sample, "all_permutations.pdf") #remove sample text from .pdf (?)
 
-def generate_permitations_for_single_char():
+def generate_permitations_for_single_char(font_holder, target_character):
     glyph_pairs_worth = remove_non_unicode_glyphs()
     __target_character__ = 44 #ascii encoded period
     target_glyph = font_holder[__target_character__]
     target_glyph_unicode_repr = chr(target_glyph.encoding)
 
-    ff.printSetup('pdf-file',"z.pdf")
+    setup_printing()
 
     print_sample = f"Permutations of {target_glyph_unicode_repr}\n"
     sample_1st_part = ""
